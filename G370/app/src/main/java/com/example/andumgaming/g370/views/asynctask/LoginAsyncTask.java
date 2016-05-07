a8a6f5d171754731f72b4766045c3d8adf926092
Pocc <whim42+github@gmail.com>
2016-05-07 14:37:14 -0700

    restoring deleted files

--- G370/app/src/main/java/com/example/andumgaming/g370/views/asynctask/LoginAsyncTask.java
+++ G370/app/src/main/java/com/example/andumgaming/g370/views/asynctask/LoginAsyncTask.java
@@ -4,29 +4,28 @@ package com.example.andumgaming.g370.views.asynctask;
  * Created by ross on 5/4/2016.
  */
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.TextView;
+import android.
 
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.Fragment;
-
-import com.example.andumgaming.g370.views.FullscreenActivity;
-import com.example.andumgaming.g370.views.fragments.MenuFragment;
-import com.example.andumgaming.g370.R;
 import com.google.gson.Gson;
-//TODO fix GSON
-//import com.google.gson.Gson;
 
 public class LoginAsyncTask extends AsyncTask<String,Void,String> {
     private TextView loginStatus;
@@ -42,59 +41,40 @@ public class LoginAsyncTask extends AsyncTask<String,Void,String> {
     }
 
     @Override
-    protected String doInBackground(String... arg0) {
+    protected String doInBackground(String... arg0)  {
+        try {
+            URL url = new URL("http://google.com");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
 
-        try{
-            String username = (String)arg0[0];
-            String password = (String)arg0[1];
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
 
-            String link="localhost:3000";
-            String data  = "/authenticate" + URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(username, "UTF-8");
-            data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
-
-            URL url = new URL(link);
-            URLConnection conn = url.openConnection();
-
-            conn.setDoOutput(true);
-            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-
-            wr.write( data );
-            wr.flush();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-
-            // Read Server Response
-            while((line = reader.readLine()) != null)
-            {
-                sb.append(line);
-                break;
-            }
-            return sb.toString();
-        }
-        catch(Exception e){
-            return new String("Exception: " + e.getMessage());
-        }
-    }
-
-    public class JsonResponse {
-        private int success;
-        private String status;
+            return result; //"Failed to fetch data!";
     }
 
     @Override
     protected void onPostExecute(String result) {
         Gson gson = new Gson();
-        JsonResponse status = gson.fromJson(result, JsonResponse.class);
-        if (status.success == 1) {
+        String json = gson.toJson(result);
+        Log.v("System.out",gson.toJson(result));
+        /*JsonResponse jsonResponse = gson.fromJson(json, JsonResponse.class);
+        if (jsonResponse.success == 1) {
             Intent i = new Intent(context, FullscreenActivity.class);
             context.startActivity(i);
             ((Activity)context).finish();
 
 
-        } else
-            this.loginStatus.setText("INVALID LOGIN");
+        } else*/
+            this.loginStatus.setText(result);
     }
 }
