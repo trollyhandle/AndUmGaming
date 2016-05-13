c2401a7a61346fc8c0b8347948dac9f67d976c93
Tyler Holland <hollandt@sonoma.edu>
2016-05-12 17:08:11 -0700

    missed a link in skipping past gameselect

--- G370/app/src/main/java/com/example/andumgaming/g370/views/fragments/PlayFragment.java
+++ G370/app/src/main/java/com/example/andumgaming/g370/views/fragments/PlayFragment.java
@@ -13,6 +13,10 @@ import com.example.andumgaming.g370.views.GameActivity;
 import com.example.andumgaming.g370.views.GameActivity;
 import com.example.andumgaming.g370.views.GameSelectActivity;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  * Created by Jeff on 4/21/2016.
  */
@@ -48,28 +52,76 @@ public class PlayFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(getActivity(), GameSelectActivity.class);
-                getActivity().startActivity(intent);
+//                Intent intent = new Intent(getActivity(), GameSelectActivity.class);
+//                getActivity().startActivity(intent);
+
+                Intent i = new Intent(getActivity(), GameActivity.class);
+
+//                Bundle extras = getActivity().getIntent().getExtras();
+//                if (extras != null && extras.getBoolean("loadJSON")) {  // then we have a json file to parse right away
+//                    InputStream is = getResources().openRawResource(R.raw.sample_game);
+//                    i.putExtra("JSON", readJSONfile(is));
+//                }
+
+                startActivity(i);
+
             }
         });
 
         join_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), GameSelectActivity.class);
-                intent.putExtra("loadJSON", true);
-                getActivity().startActivity(intent);
+//                Intent intent = new Intent(getActivity(), GameSelectActivity.class);
+//                intent.putExtra("loadJSON", true);
+//                getActivity().startActivity(intent);
+
+                Intent i = new Intent(getActivity(), GameActivity.class);
+
+//                Bundle extras = getActivity().getIntent().getExtras();
+//                if (extras != null && extras.getBoolean("loadJSON")) {  // then we have a json file to parse right away
+                InputStream is = getResources().openRawResource(R.raw.sample_game);
+                i.putExtra("JSON", readJSONfile(is));
+//                }
+
+                startActivity(i);
             }
         });
 
         watch_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), GameSelectActivity.class);
+                Intent intent = new Intent(getActivity(), GameActivity.class);
                 getActivity().startActivity(intent);
             }
         });
 
         return view;
+    }
+
+
+    public String readJSONfile(InputStream ins)
+    {
+        // File-reading code thanks to Teamnull370 (https://github.com/Teamnull370)
+        String json = "";
+        try {
+            String line;
+            StringBuffer stringBuffer = new StringBuffer();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(ins));
+
+            if (ins != null) {
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(line);
+                    stringBuffer.append("\n");
+                }
+            }
+            json = stringBuffer.toString();
+            ins.close();
+
+        }
+        catch (Exception e) {
+            //Log.e("_raws","error");
+            System.out.println("Error: " + e);
+        }
+        return json;
     }
 }
